52e047d31cada898edd919884a1cb165a4512bf3
Sheikh Siddiquee <siddiquee@hotmail.co.uk>
2022-08-24 23:56:34 +0100

    My first commit!

---
+++ test1.py
@@ -0,0 +1,6 @@
+# Function declaration
+def greet_user(first_name, last_name):
+full_name = first_name + ' ' + last_name
+print('Hey there, ' + full_name)
+# Calling the function
+greet_user('Jane', 'Doe')
